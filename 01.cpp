48f6808f81f53fecd9fa83d740b1184f8db68ff7
Sabbir Ahmed <114326056+Sabbir2110@users.noreply.github.com>
2025-11-01 22:25:38 +0600

    Refactor string analysis to use hardcoded string
    Updated the string analysis program to use a hardcoded string instead of reading from a file. Improved counting logic for words, letters, digits, and other characters.

--- 01.cpp
+++ 01.cpp
@@ -4,37 +4,45 @@ Write a C program that read the following string:
 a) Count number of words, letters, digits and other characters.
 b) Separates letters, digits and others characters.
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    freopen("01.input.txt", "r", stdin);
-    string input;
-    getline(cin, input);
+    string str = "Md. Tareq Zaman, Part-3,2011";
 
-    int word = 1, letter = 0, digit = 0, other_character = 0;
-    string letters, digits, other_characters;
+    int letters = 0, digits = 0, others = 0, words = 0;
+    string letterStr = "", digitStr = "", otherStr = "";
 
-    for(char c : input) {
-        if(c == ' ') word++; // Number of words are counted using space
-        else if(tolower(c) >= 'a' && tolower(c) <= 'z') {
-            letter++; letters += c; // Count letter and from a string
+    // Count letters, digits, and others
+    for (int i = 0; i < str.size(); i++) {
+        if (isalpha(str[i])) {
+            letters++;
+            letterStr += str[i];
+        } else if (isdigit(str[i])) {
+            digits++;
+            digitStr += str[i];
+        } else {
+            others++;
+            otherStr += str[i];
         }
-        else if(c >= '0' && c <= '9') {
-            digit++; digits += c; // Count digit and from a string
-        }
-        else {
-            other_character++; other_characters += c;
-        }
-
     }
-    cout<<"Number of words: "<<word<<endl;
-    cout<<"Number of letters: "<<letter<<endl;
-    cout<<"Number of digits: "<<digit<<endl;
-    cout<<"Number other of characters: "<<other_character<<endl;
 
-    cout<<"\nThe letters are: "<<letters<<endl;
-    cout<<"The digits are: "<<digits<<endl;
-    cout<<"The characters are: "<<other_characters<<endl;
+    // Count words
+    stringstream ss(str);
+    string word;
+    while (ss >> word) {
+        words++;
+    }
+
+    cout << "Original String: " << str << "\n\n";
+    cout << "Number of Words: " << words << endl;
+    cout << "Number of Letters: " << letters << endl;
+    cout << "Number of Digits: " << digits << endl;
+    cout << "Number of Other Characters: " << others << endl;
+
+    cout << "\nSeparated Letters: " << letterStr << endl;
+    cout << "Separated Digits: " << digitStr << endl;
+    cout << "Separated Other Characters: " << otherStr << endl;
+
     return 0;
 }
